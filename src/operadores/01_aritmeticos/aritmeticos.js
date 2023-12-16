e3986ca501a65d4b7008e5ba6a65bf9839c5be05
George Bezerra <hi@georgebezerra.dev>
2023-12-16 11:08:55 -0300

    Aula dos operadores aritmeticos

---
+++ src/operadores/01_aritmeticos/aritmeticos.js
@@ -0,0 +1,26 @@
+//valores literais
+console.log('VALORES LITERAIS')
+console.log(7 + 3);
+console.log(6 -7);
+console.log(7 * 9);
+console.log(8 / 3);
+console.log(8 % 3);
+
+console.log('##################################################')
+
+//valores por atribuição
+console.log('VALORES POR ATRIBUIÇÃO')
+let a = 8;
+let b = 11;
+
+console.log( a + b);
+console.log( a - b);
+console.log( a * b);
+console.log( a / b);
+console.log( a % b);
+
+console.log('##################################################')
+console.log('ATRIBUINDO UMA EXPRESSÃO A UMA VARIÁVEL')
+a = a + b * a / b;
+console.log(a);
+
---
+++ src/operadores/01_aritmeticos/desafio_aritmeticos.js
@@ -0,0 +1,10 @@
+//Convertendo Fareheind pra Celsius
+
+// F - 32     c
+// ------ = ---
+//    9       5
+
+let f = 73;
+let c = (f - 32) / 9 * 5;
+
+console.log(c);

e23a700e4d6346994033dd113ca36faf5537e6db
George Bezerra <hi@georgebezerra.dev>
2023-12-16 14:51:29 -0300

    Aula dos operadores atribuicao

---
+++ src/operadores/03_atribuicao/atribuicao.js
@@ -0,0 +1,24 @@
+let x = 10;
+x = 6;
+x = x + 6;
+x = x - 6;
+
+console.log(x);
+
+console.log('##################################################');
+let y = 20;
+y = 10;
+y += 10; // y = y + 10
+y -= 2; // y = y - 2
+y *= 2; // y = y * 2
+y /= 4; // y = y / 4
+y %= 3; // y = y % 3
+
+console.log(y);
+
+console.log('CONCATENAÇÃO');
+let w = 'texto';
+w += ', mais texto';
+w += '!!!';
+
+console.log(w);
---
+++ src/operadores/03_atribuicao/desafio_atribuicao.js
@@ -0,0 +1,19 @@
+let texto = 'Bom dia!\nTudo bem?';
+console.log(texto);
+console.log('##################################################');
+
+/*
+ * Lista de Aprovados
+ * 1. Ana Silva
+ * 2. Pedro Albuquerque
+ * 3. Guilherme Pereira
+ * 4. Rebeca França
+ * */
+
+let lista = '\nLista de Aprovados';
+lista += '\n==================\n\n';
+lista += '1. Ana Silva\n';
+lista += '2. Pedro Albuquerque\n';
+lista += '3. Guilherme Pereira\n';
+lista += '4. Rebeca França';
+console.log(lista);

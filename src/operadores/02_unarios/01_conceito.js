63ecd3fba0ee2c01af0720d134f9549c1a56a77f
George Bezerra <hi@georgebezerra.dev>
2023-12-18 15:06:22 -0300

    Aula de operadores unários

---
+++ src/operadores/02_unarios/01_conceito.js
@@ -0,0 +1,27 @@
+console.log('OPERADOR DE ATRIBUIÇÃO');
+let a = 1;
+console.log('atribuindo ' + a + ' variável "a": \n');
+
+console.log('OPERADOR DE ARITMÉTICO SIMPLES');
+a = a + 1;
+console.log('atribuindo ' + a + ' variável "a": \n');
+
+console.log('OPERADOR DE ATRIBUIÇÃO ARITMÉTICO - ADITIVA');
+a += 1;
+console.log('atribuindo ' + a + ' variável "a": \n');
+
+console.log('OPERADOR UNÁRIO POSTFIX DE ATRIBUIÇÃO - INCREMENTO');
+a++;
+console.log('atribuindo ' + a + ' variável "a": \n');
+
+console.log('OPERADOR UNÁRIO PREFIX DE ATRIBUIÇÃO - INCREMENTO');
+++a;
+console.log('atribuindo ' + a + ' variável "a": \n');
+
+console.log('OPERADOR UNÁRIO POSTFIX DE ATRIBUIÇÃO - DECREMENTO');
+a--;
+console.log('atribuindo ' + a + ' variável "a": \n');
+
+console.log('OPERADOR UNÁRIO PREFIX DE ATRIBUIÇÃO - DECREMENTO');
+--a;
+console.log('atribuindo ' + a + ' variável "a": \n');
---
+++ src/operadores/02_unarios/03_unarios.js
@@ -0,0 +1,17 @@
+console.log('NEGAÇÃO');
+console.log(!true);
+console.log(!false);
+
+console.log('\nAFIRMAÇÃO');
+console.log(!!true);
+console.log(!!false);
+
+console.log('\nSTRING - AFIRMAÇÃO');
+console.log(!'texto!');
+console.log(!!'texto!');
+
+console.log('\nNUMBER - AFIRMAÇÃO - TODOS OS NÚMEROS SERÃO TRUE EXCETO 0');
+console.log(!!0);
+console.log(!!1);
+console.log(!!-1);
+console.log(!!-0.00001);

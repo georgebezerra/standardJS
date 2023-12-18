ecbaec5ed0f6417b546b829b57adc50b264bf685
George Bezerra <hi@georgebezerra.dev>
2023-12-18 15:59:03 -0300

    Aula de operadores ternário

---
+++ src/operadores/03_ternario/01_ternario.js
@@ -0,0 +1,2 @@
+console.log('TERNÁRIO: 1 parte  ? 2 parte : 3 parte');
+console.log('SINTAXE: expressão ? true : false');
---
+++ src/operadores/03_ternario/02_ternario.js
@@ -0,0 +1,9 @@
+const hora = 14;
+const saudacao = hora <= 11 ? 'Bom dia' : 'Boa tarde';
+console.log(saudacao);
+
+console.log('\n===============================\n');
+
+const hora02 = 23;
+const sauda = hora02 <= 11 ? 'Bom dia' : hora02 <= 17 ? 'Bom dia' : 'Boa tarde';
+console.log(sauda);

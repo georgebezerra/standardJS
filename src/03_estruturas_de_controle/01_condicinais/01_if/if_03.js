ec1ffddb18ded5d2a272b9c5db1071bb98a2b22d
George Bezerra <hi@georgebezerra.dev>
2023-12-20 11:32:10 -0300

    Aula de condicinais - IF - Bloco de código com proposíto - associado a uma estrutura de controle.

--- src/03_estruturas_de_controle/01_condicinais/01_if/if_03.js
+++ src/03_estruturas_de_controle/01_condicinais/01_if/if_03.js
@@ -5,11 +5,12 @@ if (nota >= 9) console.log('Quadro de Honra!');
 console.log('Parabéns\n');
 
 console.log('Estrutura de controle CORRETA - FALSE - A ESTRUTURA NÃO EXECUTA');
-console.log(
-  'BLOCO DE CÓDIGO COM PROPOSITO - ASSOCIADO A UMA ESTRUTURA DE CONTROLE',
-);
+
 if (nota >= 9) {
-  console.log('Quadro de Honra!');
+  console.log(
+    'BLOCO DE CÓDIGO COM PROPOSITO - ASSOCIADO A UMA ESTRUTURA DE CONTROLE',
+  );
+  console.log('Quadro de Honra!\n');
   console.log('Parabéns\n');
 }
 

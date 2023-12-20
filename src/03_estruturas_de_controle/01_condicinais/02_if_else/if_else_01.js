df754e4f68d335a8cf1ae3df02035693e16108b9
George Bezerra <hi@georgebezerra.dev>
2023-12-20 14:48:04 -0300

    Aula de condicinais - IF - ELSE

---
+++ src/03_estruturas_de_controle/01_condicinais/02_if_else/if_else_01.js
@@ -0,0 +1,11 @@
+const verdadeira = true;
+const falsa = false;
+
+if (verdadeira) console.log('Exec...');
+else console.log('Não exec...');
+
+if (falsa) {
+  console.log('Exec...');
+} else {
+  console.log('Não exec...');
+}
---
+++ src/03_estruturas_de_controle/01_condicinais/02_if_else/if_else_04.js
@@ -0,0 +1,14 @@
+const hora = 23;
+let saudacao;
+
+if (hora < 12) {
+  saudacao = 'Bom dia!';
+} else if (hora < 18) {
+  saudacao = 'Boa tarde!';
+} else if (hora < 22) {
+  saudacao = 'Boa tarde!';
+} else {
+  saudacao = 'Muita tarde!';
+}
+
+console.log(saudacao);

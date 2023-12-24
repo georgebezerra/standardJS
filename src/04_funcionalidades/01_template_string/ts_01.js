19b2037ba522571db7ab8c00b593736f260cafbc
George Bezerra <hi@georgebezerra.dev>
2023-12-24 12:06:58 -0300

    Aula Template String.

---
+++ src/04_funcionalidades/01_template_string/ts_01.js
@@ -0,0 +1,12 @@
+const i = 2;
+const j = 4;
+
+console.log('USANDO CONCATENAÇÃO');
+console.log('i = ' + i + ', j = ' + j);
+
+console.log('\nUSANDO TEMPLATE STRING - BACKTICK');
+console.log(`i = ${i}, j = ${j}`);
+
+console.log('\nMAIS EXEMPLO');
+console.log(`1 + 2 = ${1 + 2}`);
+console.log(`Valor aleatório gerado: ${Math.random()}`);

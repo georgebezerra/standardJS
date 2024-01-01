0f3bcb087634babfd73c45bdade8aeef4afe3ed6
George Bezerra <hi@georgebezerra.dev>
2024-01-01 15:36:11 -0300

    4 variações de funções e desafio.

--- src/05_funcoes/4_variacoes_de_funcoes.js
+++ src/05_funcoes/4_variacoes_de_funcoes.js
@@ -1,26 +1,23 @@
-// #1: função COM parâmetros e COM retorno
+// #1-VARIAÇÃO: função COM parâmetros e COM retorno
 function adicao(a, b) {
   return a + b;
 }
 console.log(adicao(1, 2));
 
-// #2: função COM parâmetro e SEM retorno
+// #2-VARIAÇÃO: função COM parâmetro e SEM retorno
 function multiplicacao(a, b) {
   console.log(a * b);
 }
-
 multiplicacao(3, 4);
 
-// #3: função SEM parâmetro e COM retorno
+// #3-VARIAÇÃO: função SEM parâmetro e COM retorno
 function retornarDataAtual() {
   return new Date();
 }
-
 console.log(retornarDataAtual());
 
-// #4:função SEM parâmetro e SEM retorno
+// #4-VARIAÇÃO:função SEM parâmetro e SEM retorno
 function exebirHoraAtual() {
   console.log(new Date().getHours());
 }
-
 exebirHoraAtual();

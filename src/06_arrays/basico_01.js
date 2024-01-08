5d76fd38875eedf2a591695559335ef73dcc9846
George Bezerra <hi@georgebezerra.dev>
2024-01-07 21:36:55 -0300

    Aula de array; declarando um array, acessando elementos de um array e alterando o valor de um array.

---
+++ src/06_arrays/basico_01.js
@@ -0,0 +1,12 @@
+//Declarando um array
+const notas = [7.8, 6.7, 10, 9.5, 7.8, 5.8];
+console.log(notas);
+console.log(typeof notas);
+
+//Acessando elementos do array
+console.log(notas[3]);
+console.log(notas[500]);
+
+//Alterando o valor de um array
+notas[4] = 8.7;
+console.log(notas);

73cc0e837fc6dfdb41268c0af966502b2f5139b9
George Bezerra <hi@georgebezerra.dev>
2024-01-08 18:18:40 -0300

    Aula de array; Todo array é um Obejct especial do JS.

---
+++ src/06_arrays/basico_02.js
@@ -0,0 +1,13 @@
+const nomes = [];
+console.log(nomes);
+console.log(typeof nomes);
+
+console.log('\nInserindo novos valores nos indices');
+nomes[0] = 'Pedro';
+nomes[1] = 'Ana';
+nomes[2] = 'Bia';
+nomes[3] = 'Guia';
+nomes[4] = 'Rebeca';
+nomes[10] = 'Tiago';
+
+console.log(nomes);

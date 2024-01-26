6e11308203f3ed152e02b19b0db74cdc72f0ce73
George Bezerra <hi@georgebezerra.dev>
2024-01-26 19:43:36 -0300

    02 - array.length - add doc link.

--- src/08_arrays_methods/existente/02_arrasy_length.js
+++ src/08_arrays_methods/existente/02_arrasy_length.js
@@ -1,3 +1,4 @@
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/length
 //length(): This property returns the length o the array.
 const arr = ['apple', 'banana', 'cherry'];
 const frutas = arr.length;

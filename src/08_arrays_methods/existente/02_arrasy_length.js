c426a9e08335fcb797602fb5e23d70ccdb92002e
George Bezerra <hi@georgebezerra.dev>
2024-01-29 19:59:54 -0300

    03 - array.reverse - add doc link.

--- src/08_arrays_methods/existente/02_arrasy_length.js
+++ src/08_arrays_methods/existente/02_arrasy_length.js
@@ -1,6 +1,13 @@
 //https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/length
 //length(): This property returns the length o the array.
+
+console.log('Array de frutas na ordem default:');
 const arr = ['apple', 'banana', 'cherry'];
+console.log(arr);
+
+console.log(
+  '\nArray de frutas com length() - comprimento do array implementado:',
+);
 const frutas = arr.length;
 
 console.log(frutas);
---
+++ src/08_arrays_methods/existente/03_array_reverse.js
@@ -0,0 +1,11 @@
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse
+//reverse(): This method reverses the order of the elements in the array
+
+console.log('Array de frutas na ordem default:');
+const arr = ['apple', 'banana', 'cherry'];
+console.log(arr);
+
+const itemreverve = arr.reverse();
+
+console.log('\nArray de frutas com reverse() implementado:');
+console.log(itemreverve);

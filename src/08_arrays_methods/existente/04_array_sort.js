bff74a707e88d5bd82475ed6af6ee7f1161623b6
George Bezerra <hi@georgebezerra.dev>
2024-01-30 20:59:17 -0300

    03 - array.sort - add doc link.

---
+++ src/08_arrays_methods/existente/04_array_sort.js
@@ -0,0 +1,11 @@
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+//sort(): This method sorts the elements of an array in place and returns the sorted array.
+//It can take an optional compare function as an argument.
+
+console.log('Array de frutas na ordem default:');
+const arr = ['banana', 'apple', 'cherry'];
+console.log(arr);
+
+console.log('\nArray de frutas com o sort() sendo exetudado.');
+const itemsort = arr.sort();
+console.log(itemsort);

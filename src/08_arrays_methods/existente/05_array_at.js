a91e048256a7479091f8e086a3de035eb21a48d7
George Bezerra <hi@georgebezerra.dev>
2024-01-31 22:03:54 -0300

    03 - array.at - add doc link.

---
+++ src/08_arrays_methods/existente/05_array_at.js
@@ -0,0 +1,11 @@
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/at
+//at(): Thie method retuns the element at the specified index the array.It takes one argument: the index.
+
+console.log('Array de frustas default');
+const arr = ['apple', 'banana', 'cherry'];
+console.log(arr);
+
+console.log(
+  '\nArray de frutas com o método at() - será retornado o elemento baseado no indice.',
+);
+console.log(arr.at(1));

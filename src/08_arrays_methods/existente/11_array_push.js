0c798fe43b33ab5d2231bd959a38c5dfc4b5738c
George Bezerra <hi@georgebezerra.dev>
2024-02-15 20:03:58 -0300

    método push() e adjust na doc.

---
+++ src/08_arrays_methods/existente/11_array_push.js
@@ -0,0 +1,11 @@
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/push
+//push(): O método push() adiciona um ou mais elementos ao final de um array e retorna o novo comprimento desse array.
+//method adds one or more elements to the end of an array and returns the new length of the array.
+
+console.log('Array de frutas');
+let fruits = ['apple', 'banana'];
+console.log(fruits);
+
+console.log('\nAcrescentando 2 frutas');
+fruits.push('cherry', 'orange');
+console.log(fruits);

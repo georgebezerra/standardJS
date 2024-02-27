fcffb1e8728267567b1bad842dabed50b51be718
George Bezerra <hi@georgebezerra.dev>
2024-02-27 19:13:20 -0300

    método of() e adjust na doc.

---
+++ src/08_arrays_methods/novo/08_array_of.js
@@ -0,0 +1,8 @@
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/of
+//O método Array.of() cria um nova instância de Array com um número variável de argumentos, independentemente do número ou do tipo dos argumentos.
+//This method creates a new array instance with a variable number of arguments, regardless of number or types of the arguments.
+
+console.log('Array criado com of');
+const arr = Array.of(1, 2, 3, 'four', true);
+console.log(arr);
+console.log('Fim');

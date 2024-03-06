840bda3177db840d1b9af0b8baf59f4eb7489702
George Bezerra <hi@georgebezerra.dev>
2024-03-06 17:02:59 -0300

    método includes() e adjust na doc.

---
+++ src/08_arrays_methods/existente/17_array_includes.js
@@ -0,0 +1,8 @@
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+//O método includes() determina se um array contém um determinado elemento, retornando true ou false apropriadamente.
+//This method determines whether an array includes a certain value among its entries, returning true of false as appropriate.
+console.log('Array default');
+const arr = [10, 20, 30, 40, 50];
+const has20 = arr.includes(20);
+console.log('Array com includes implementado');
+console.log(has20);
